834a2a747997bb9762981dc25a87e21d071bbb40
Kasia Napieralska <k.napieralska@gmail.com>
2023-09-21 22:59:11 +0100

    Update

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const circle = document.querySelector(".heart");
 const letter = document.querySelector(".letter");
 const envelope = document.querySelector(".envelope-open");
 const h1 = document.querySelector("h1");
+const video = document.querySelector("video");
 
 console.log(envelope);
 
@@ -9,7 +10,9 @@ console.log(envelope);
 function openLtter() {
     letter.classList.toggle('active');
     circle.classList.toggle('heart-active');
+    video.play();
     if (circle.classList.contains('heart-active')) {
+
         h1.classList.remove('changeTwo')
         h1.classList.add('change')
         envelope.classList.remove("close");
